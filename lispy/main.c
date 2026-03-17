a4f7de46463f87b44f7f9e57cb81088bdbb5a2ac
adambanner1904 <120569454+adambanner1904@users.noreply.github.com>
2026-03-17 08:59:24 +0000

    Finished chapter 8: error handling added along with types

--- lispy/main.c
+++ lispy/main.c
@@ -5,25 +5,82 @@
 #include "mpc.h"
 #include <editline/readline.h>
 
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    return 0;
+typedef struct {
+    int type;
+    long num;
+    int err;
+} LispValue;
+
+// Enum of lval types
+enum {LVAL_NUM, LVAL_ERR};
+// Enum of lval error types
+enum {LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
+
+// type constructors
+LispValue Number(long x) {
+    LispValue v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t* t) {
+LispValue Error(int x) {
+    LispValue v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+void lval_print(LispValue v) {
+    switch (v.type) {
+        case LVAL_NUM: printf("%li", v.num); break;
+
+        case LVAL_ERR:
+            if (v.err == LERR_DIV_ZERO) {
+                printf("Error: Division by zero");
+            }
+            if (v.err == LERR_BAD_OP) {
+                printf("Error: Invalid operation");
+            }
+            if (v.err == LERR_BAD_NUM) {
+                printf("Error: Invalid number");
+            }
+            break;
+    }
+}
+
+void lval_println(LispValue v) {
+    lval_print(v); putchar('\n');
+}
+
+LispValue eval_op(LispValue x, char* op, LispValue y) {
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
+
+    if (strcmp(op, "+") == 0) { return Number(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return Number(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return Number(x.num * y.num); }
+    if (strcmp(op, "/") == 0) {
+
+        return y.num == 0
+            ? Error(LERR_DIV_ZERO)
+            : Number(x.num / y.num);
+    }
+    return Error(LERR_BAD_OP);
+}
+
+LispValue eval(mpc_ast_t* t) {
     // if number in tag return number: base case
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? Number(x) : Error(LERR_BAD_NUM);
     }
 
     // operator is always second child
     char* op = t->children[1]->contents;
-
     // store the third child in x: could be a tree so eval it
-    long x = eval(t->children[2]);
+    LispValue x = eval(t->children[2]);
 
     int i = 3;
     while (strstr(t->children[i]->tag, "expr")) {
@@ -54,8 +111,8 @@ int main(int argc, char *argv[]) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            LispValue result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
