c02d18d131af90e3fa6c7ffcb0c8281419c6fcc3
adambanner1904 <120569454+adambanner1904@users.noreply.github.com>
2026-03-17 07:49:25 +0000

    Starting from scratch (on chapter 6) to understand what is happening more deeply

--- lispy/main.c
+++ lispy/main.c
@@ -1,37 +1,43 @@
-#include <stdlib.h>
+//
+// Created by Developer on 17/03/2026.
+//
 
+#include "mpc.h"
 #include <editline/readline.h>
 
-#include "eval.h"
-#include "lval.h"
-#include "parsing.h"
+int main(int argc, char *argv[]) {
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
 
-int main(int argc, char** argv) {
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "number   : /-?[0-9]+/ ;    "
+        "operator : '+' | '-' | '*' | '/' ;"
+        "expr     : <number> | '(' <operator> <expr>+ ')' ;"
+        "lispy     : /^/ <operator> <expr>+ /$/ ;",
+        Number, Operator, Expr, Lispy);
 
-    parsers_init();
-    lenv* e = lenv_new();
-    lenv_add_builtins(e);
-
-    puts("Lispy Version 0.0.0.0.1");
-    puts("Press Ctrl+c to Exit");
-
-
+    puts("Lispy Version 0.0.0.0.2");
+    puts("Press Ctrl+c to Exit\n");
     while (1) {
-
         char* input = readline("lispy> ");
-        if (!input) break;
         add_history(input);
 
-        lval* parsed_input = parse_input("<stdin>", input);
-        lval* evaluated_input = lval_eval(e, parsed_input);
-
-        lval_println(evaluated_input);
-
-        lval_del(evaluated_input);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
+
     }
-    lenv_del(e);
-    parsers_cleanup();
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+
     return 0;
 }
