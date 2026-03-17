1014ffff90e3f220b1e0f39a9521630863643924
adambanner1904 <120569454+adambanner1904@users.noreply.github.com>
2026-03-17 08:16:46 +0000

    Finished chapter 7 and working

--- lispy/main.c
+++ lispy/main.c
@@ -5,6 +5,34 @@
 #include "mpc.h"
 #include <editline/readline.h>
 
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    // if number in tag return number: base case
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    // operator is always second child
+    char* op = t->children[1]->contents;
+
+    // store the third child in x: could be a tree so eval it
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
+
 int main(int argc, char *argv[]) {
     mpc_parser_t* Number = mpc_new("number");
     mpc_parser_t* Operator = mpc_new("operator");
@@ -26,7 +54,8 @@ int main(int argc, char *argv[]) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
